89e1da5a72741315075b2a1271435bf0afcdff0d
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-09-08 22:03:50 +0500

    update

--- MediQueue/MediQueue.Domain/DTOs/Permission/PermissionDto.cs
+++ MediQueue/MediQueue.Domain/DTOs/Permission/PermissionDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MediQueue.Domain.DTOs.Permission
+﻿namespace MediQueue.Domain.DTOs.Permission
 {
     public record PermissionDto(int Id, string Name, string Controller, string Action);
 }
