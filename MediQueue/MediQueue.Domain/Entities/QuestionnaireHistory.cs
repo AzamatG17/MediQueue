8ff172f3efa957aaef1341b95b40122ce328f1b4
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-09-11 14:40:47 +0500

    Update Ip

--- MediQueue/MediQueue.Domain/Entities/QuestionnaireHistory.cs
+++ MediQueue/MediQueue.Domain/Entities/QuestionnaireHistory.cs
@@ -6,12 +6,14 @@ namespace MediQueue.Domain.Entities
     {
         public int Historyid { get; set; }
         public string? HistoryDiscription { get; set; }
-        public DateTime DateCreated { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public decimal? Balance { get; set; }
+        public bool? IsPayed { get; set; }
 
-        public int AccountId { get; set; }
-        public Account Account { get; set; }
-        public int QuestionnaireId { get; set; }
-        public Questionnaire Questionnaire { get; set; }
+        public int? AccountId { get; set; }
+        public Account? Account { get; set; }
+        public int? QuestionnaireId { get; set; }
+        public Questionnaire? Questionnaire { get; set; }
 
         public virtual ICollection<Service> Services { get; set; }
     }
