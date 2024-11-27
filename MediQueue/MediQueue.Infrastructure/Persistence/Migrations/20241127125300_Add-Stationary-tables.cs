b64fe741b7dee3985066f4955ef6d4bb7a955b63
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-11-27 17:56:29 +0500

    Add migrations

---
+++ MediQueue/MediQueue.Infrastructure/Persistence/Migrations/20241127125300_Add-Stationary-tables.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediQueue.Infrastructure.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStationarytables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nutritions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MealPlan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CostPerDay = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nutritions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tariff",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    PricePerDay = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tariff", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ward",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WardName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ward", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StationaryStay",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    NumberOfDays = table.Column<int>(type: "int", nullable: true),
+                    TariffId = table.Column<int>(type: "int", nullable: true),
+                    WardPlaceId = table.Column<int>(type: "int", nullable: true),
+                    NutritionId = table.Column<int>(type: "int", nullable: true),
+                    QuestionnaireHistoryId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StationaryStay", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StationaryStay_Nutritions_NutritionId",
+                        column: x => x.NutritionId,
+                        principalTable: "Nutritions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_StationaryStay_QuestionnaireHistory_QuestionnaireHistoryId",
+                        column: x => x.QuestionnaireHistoryId,
+                        principalTable: "QuestionnaireHistory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_StationaryStay_Tariff_TariffId",
+                        column: x => x.TariffId,
+                        principalTable: "Tariff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WardPlace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WardPlaceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsOccupied = table.Column<bool>(type: "bit", nullable: true, defaultValue: false),
+                    WardId = table.Column<int>(type: "int", nullable: true),
+                    StationaryStayId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WardPlace", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WardPlace_StationaryStay_StationaryStayId",
+                        column: x => x.StationaryStayId,
+                        principalTable: "StationaryStay",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_WardPlace_Ward_WardId",
+                        column: x => x.WardId,
+                        principalTable: "Ward",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StationaryStay_NutritionId",
+                table: "StationaryStay",
+                column: "NutritionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StationaryStay_QuestionnaireHistoryId",
+                table: "StationaryStay",
+                column: "QuestionnaireHistoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StationaryStay_TariffId",
+                table: "StationaryStay",
+                column: "TariffId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WardPlace_StationaryStayId",
+                table: "WardPlace",
+                column: "StationaryStayId",
+                unique: true,
+                filter: "[StationaryStayId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WardPlace_WardId",
+                table: "WardPlace",
+                column: "WardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WardPlace");
+
+            migrationBuilder.DropTable(
+                name: "StationaryStay");
+
+            migrationBuilder.DropTable(
+                name: "Ward");
+
+            migrationBuilder.DropTable(
+                name: "Nutritions");
+
+            migrationBuilder.DropTable(
+                name: "Tariff");
+        }
+    }
+}
