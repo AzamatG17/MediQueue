7edbbeadf739ab07f7be9ffcdfe671892206fa1a
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-11-27 17:55:01 +0500

    Add mappings

---
+++ MediQueue/MediQueue.Domain/Mappings/TariffMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediQueue.Domain.DTOs.Tariff;
+using MediQueue.Domain.Entities;
+
+namespace MediQueue.Domain.Mappings
+{
+    public class TariffMapping : Profile
+    {
+        public TariffMapping()
+        {
+            CreateMap<Tariff, TariffDto>();
+            CreateMap<TariffDto, Tariff>();
+            CreateMap<TariffForCreateDto, Tariff>();
+            CreateMap<TariffForUpdateDto, Tariff>();
+        }
+    }
+}
