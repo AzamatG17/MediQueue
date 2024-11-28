683ecd5bd937672c60becd894a231727be43f6ad
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-11-28 17:23:26 +0500

    add migrations

---
+++ MediQueue/MediQueue.Infrastructure/Persistence/Migrations/20241128112658_update-Tariff-Ward.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediQueue.Infrastructure.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTariffWard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalCost",
+                table: "StationaryStay",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TariffWard",
+                columns: table => new
+                {
+                    TariffsId = table.Column<int>(type: "int", nullable: false),
+                    WardsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TariffWard", x => new { x.TariffsId, x.WardsId });
+                    table.ForeignKey(
+                        name: "FK_TariffWard_Tariff_TariffsId",
+                        column: x => x.TariffsId,
+                        principalTable: "Tariff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TariffWard_Ward_WardsId",
+                        column: x => x.WardsId,
+                        principalTable: "Ward",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TariffWard_WardsId",
+                table: "TariffWard",
+                column: "WardsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TariffWard");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "StationaryStay");
+        }
+    }
+}
