76f67826f69672de399cfb5f06b2871eb5d0d195
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-13 16:31:43 +0500

    Update IServices

---
+++ MediQueue/MediQueue.Domain/Interfaces/Services/IAuditLogService.cs
@@ -0,0 +1,10 @@
+﻿using MediQueue.Domain.Entities;
+
+namespace MediQueue.Domain.Interfaces.Services;
+
+public interface IAuditLogService
+{
+    Task<IEnumerable<AuditLog>> GetAllLogAsync();
+    Task<IEnumerable<AuditLog>> GetByIdLogsAsync(int id);
+    Task LogAsync(AuditLog log);
+}
--- MediQueue/MediQueue.Domain/Interfaces/Services/ILekarstvoService.cs
+++ MediQueue/MediQueue.Domain/Interfaces/Services/ILekarstvoService.cs
@@ -1,10 +1,11 @@
 ﻿using MediQueue.Domain.DTOs.Lekarstvo;
+using MediQueue.Domain.ResourceParameters;
 
 namespace MediQueue.Domain.Interfaces.Services;
 
 public interface ILekarstvoService
 {
-    Task<IEnumerable<LekarstvoDto>> GetAllLekarstvosAsync();
+    Task<IEnumerable<LekarstvoDto>> GetAllLekarstvosAsync(LekarstvoResourceParametrs lekarstvoResourceParametrs);
     Task<LekarstvoDto> GetLekarstvoByIdAsync(int id);
     Task<LekarstvoDto> CreateLekarstvoAsync(LekarstvoForCreateDto lekarstvoForCreateDto);
     Task<LekarstvoDto> UpdateLekarstvoAsync(LekarstvoForUpdateDto lekarstvoForUpdateDto);
