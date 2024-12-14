23d0c7749f00c536b45e3fc1e83a2f0eb8f2c0da
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-14 11:07:58 +0500

    Add AuditLogResourseParament for filter data

---
+++ MediQueue/MediQueue.Domain/ResourceParameters/AuditLogResourceParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace MediQueue.Domain.ResourceParameters;
+
+public class AuditLogResourceParameters : ResourceParametersBase
+{
+    public int? Id { get; set; }
+    public string? TableName { get; set; }
+    public DateTime? StartDateTime { get; set; }
+    public DateTime? EndDateTime { get; set; }
+    public int? AccountId { get; set; }
+    public Action? Action { get; set; }
+    public override string OrderBy { get; set; } = "idDesc";
+}
+
+public enum Action
+{
+    Added,
+    Modified,
+    Deleted
+}
