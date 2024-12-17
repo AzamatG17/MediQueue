0d724c60226dc0910fe47e8a9fc7580c7151161f
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-17 16:38:04 +0500

    Add Configuration for new tables

---
+++ MediQueue/MediQueue.Infrastructure/Persistence/Configurations/ProcedureBookingConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using MediQueue.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediQueue.Infrastructure.Persistence.Configurations
+{
+    internal class ProcedureBookingConfiguration : IEntityTypeConfiguration<ProcedureBooking>
+    {
+        public void Configure(EntityTypeBuilder<ProcedureBooking> builder)
+        {
+            builder.ToTable(nameof(ProcedureBooking));
+            builder.HasKey(x => x.Id);
+
+            builder.Property(e => e.BookingDate)
+                .HasColumnType("datetime");
+
+            builder.HasOne(e => e.Procedure)
+                .WithMany(pb => pb.ProcedureBookings)
+                .HasForeignKey(e => e.ProcedureId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(e => e.StationaryStayUsage)
+                .WithMany(pb => pb.ProcedureBookings)
+                .HasForeignKey(e => e.StationaryStayUsageId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
---
+++ MediQueue/MediQueue.Infrastructure/Persistence/Configurations/ProcedureCategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using MediQueue.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediQueue.Infrastructure.Persistence.Configurations
+{
+    internal class ProcedureCategoryConfiguration : IEntityTypeConfiguration<ProcedureCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProcedureCategory> builder)
+        {
+            builder.ToTable(nameof(ProcedureCategory));
+            builder.HasKey(x => x.Id);
+
+            builder.Property(a => a.Name)
+                .HasMaxLength(255);
+
+            builder.HasMany(e => e.Procedures)
+                .WithOne(p => p.ProcedureCategory)
+                .HasForeignKey(p => p.ProcedureCategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
