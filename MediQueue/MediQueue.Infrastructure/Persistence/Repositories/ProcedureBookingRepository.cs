6208be1f319bbe7f87ed09b7d1defa25b51738d9
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-17 16:37:38 +0500

    Add Repositories

---
+++ MediQueue/MediQueue.Infrastructure/Persistence/Repositories/ProcedureBookingRepository.cs
@@ -0,0 +1,34 @@
+﻿using MediQueue.Domain.Entities;
+using MediQueue.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediQueue.Infrastructure.Persistence.Repositories
+{
+    public class ProcedureBookingRepository : RepositoryBase<ProcedureBooking>, IProcedureBookingRepository
+    {
+        public ProcedureBookingRepository(MediQueueDbContext mediQueueDbContext)
+            : base(mediQueueDbContext)
+        {
+        }
+
+        public async Task<IEnumerable<ProcedureBooking>> FandAllProcedureBookingAsync()
+        {
+            return await _context.ProcedureBookings
+                .Include(p => p.Procedure)
+                .Include(s => s.StationaryStayUsage)
+                .Where(x => x.IsActive)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<ProcedureBooking> FindByIdProcedureBookingAsync(int id)
+        {
+            return await _context.ProcedureBookings
+                .Include(p => p.Procedure)
+                .Include(s => s.StationaryStayUsage)
+                .Where(x => x.Id == id && x.IsActive)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+        }
+    }
+}
