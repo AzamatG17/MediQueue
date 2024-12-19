16424343f651dd7f65b001ba83ed4487f0ca8c04
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-19 17:29:19 +0500

    Update Services

--- MediQueue/MediQueue.Domain/Interfaces/Services/IProcedureService.cs
+++ MediQueue/MediQueue.Domain/Interfaces/Services/IProcedureService.cs
@@ -1,10 +1,11 @@
 ﻿using MediQueue.Domain.DTOs.Procedure;
+using MediQueue.Domain.ResourceParameters;
 
 namespace MediQueue.Domain.Interfaces.Services;
 
 public interface IProcedureService
 {
-    Task<IEnumerable<ProcedureDto>> GetAllProceduresAsync();
+    Task<IEnumerable<ProcedureDto>> GetAllProceduresAsync(ProcedureResourceParameters procedureResourceParameters);
     Task<ProcedureDto> GetProcedureByIdAsync(int id);
     Task<ProcedureDto> CreateProcedureAsync(ProcedureForCreateDto procedureForCreateDto);
     Task<ProcedureDto> UpdateProcedureAsync(ProcedureForUpdateDto procedureForUpdateDto);
