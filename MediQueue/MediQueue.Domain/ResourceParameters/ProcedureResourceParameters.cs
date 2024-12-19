c8c87c0da7702b326023ea0f49081b4613c6d88f
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-19 17:27:48 +0500

    Create Procedure Resource Parametr

---
+++ MediQueue/MediQueue.Domain/ResourceParameters/ProcedureResourceParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace MediQueue.Domain.ResourceParameters;
+
+public class ProcedureResourceParameters : ResourceParametersBase
+{
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public override string OrderBy { get; set; } = "idDesc";
+}
