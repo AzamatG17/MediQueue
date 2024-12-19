e980c4a27ac866c45d7c441d7080c0280e93bd1f
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-12-19 17:28:21 +0500

    Add Migrations

---
+++ MediQueue/MediQueue.Infrastructure/Persistence/Migrations/20241219101139_Add-Procedure.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediQueue.Infrastructure.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcedureCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcedureCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Procedure",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartTime = table.Column<TimeOnly>(type: "TIME", nullable: false),
+                    EndTime = table.Column<TimeOnly>(type: "TIME", nullable: false),
+                    MaxPatients = table.Column<int>(type: "int", nullable: false),
+                    ProcedureCategoryId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Procedure", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Procedure_ProcedureCategory_ProcedureCategoryId",
+                        column: x => x.ProcedureCategoryId,
+                        principalTable: "ProcedureCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProcedureBooking",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookingDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ProcedureId = table.Column<int>(type: "int", nullable: true),
+                    StationaryStayUsageId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcedureBooking", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProcedureBooking_Procedure_ProcedureId",
+                        column: x => x.ProcedureId,
+                        principalTable: "Procedure",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_ProcedureBooking_StationaryStayUsage_StationaryStayUsageId",
+                        column: x => x.StationaryStayUsageId,
+                        principalTable: "StationaryStayUsage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Procedure_ProcedureCategoryId",
+                table: "Procedure",
+                column: "ProcedureCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcedureBooking_ProcedureId",
+                table: "ProcedureBooking",
+                column: "ProcedureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcedureBooking_StationaryStayUsageId",
+                table: "ProcedureBooking",
+                column: "StationaryStayUsageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcedureBooking");
+
+            migrationBuilder.DropTable(
+                name: "Procedure");
+
+            migrationBuilder.DropTable(
+                name: "ProcedureCategory");
+        }
+    }
+}
